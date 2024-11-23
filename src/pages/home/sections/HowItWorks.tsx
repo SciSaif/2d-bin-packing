5f378434b5e145e900f52cb28d5a0abb603af6d4
A1Kumari <aryakumari3953@gmail.com>
2024-11-23 14:10:41 +0530

    added all the gifs

--- src/pages/home/sections/HowItWorks.tsx
+++ src/pages/home/sections/HowItWorks.tsx
@@ -7,29 +7,34 @@ const HowItWorks: React.FC = () => {
       title: "Choose Your Method",
       description:
         "Select either Freeform Mode for manual customization or Efficient Mode for automatic arrangement of your images.",
+      imgPath: "step1.gif", 
     },
     {
       number: "02",
       title: "Upload Your Images",
       description: "Drag, drop, or upload images directly into the app to get started.",
+      imgPath: "step2.gif", 
     },
     {
       number: "03",
       title: "Arrange and Customize",
       description:
         "Optimize the layout by arranging your images and tweaking settings to match your preferences.",
+      imgPath: "step3.gif", 
     },
     {
       number: "04",
-      title: "Preview the Layout",
+      title: "Efficient Packing Algorithm",
       description:
-        "Review your arranged layout and make final adjustments to ensure it meets your needs.",
-    },
+        "Our algorithm will arrange your images efficiently. Review the layout and make final adjustments as needed.",
+      imgPath: "step4.gif", 
+    },    
     {
       number: "05",
-      title: "Save or Share",
+      title: "Save or Print",
       description:
-        "Save your final work as a PDF, print it directly, or share it with others.",
+        "Save your final work as a PDF and print it directly",
+      imgPath: "step5.gif", 
     },
   ];
 
@@ -42,10 +47,20 @@ const HowItWorks: React.FC = () => {
             key={index}
             className="relative bg-teal-50 text-gray-800 p-6 rounded-xl shadow-lg mt-6"
           >
+            {/* Image (GIF) */}
+            <div className="mb-4">
+              <img
+                src={step.imgPath}
+                alt={`Step ${step.number}`}
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+
             {/* Number Box */}
             <div className="absolute -top-6 left-6 bg-teal-600 text-white font-extrabold text-xl w-12 h-12 flex items-center justify-center rounded-lg shadow-md">
               {step.number}
             </div>
+
             {/* Content */}
             <div className="mt-8">
               <h3 className="text-lg font-semibold text-teal-800 mb-2">
